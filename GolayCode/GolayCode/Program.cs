d527041bf8240ff35b26bed2c4caed9f42ef1bea
Matas Savickis <savickismatas@gmail.com>
2018-09-07 23:46:32 +0300

    Initial commit

---
+++ GolayCode/GolayCode/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GolayCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Golay Code project");
+            Console.ReadLine();
+        }
+    }
+}
